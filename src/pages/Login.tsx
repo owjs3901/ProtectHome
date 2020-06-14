1b2e39d23ee071a0a63515d3e770cc0e1e7714d8
M <owjs3901@naver.com>
2020-06-15 00:48:00 +0900

    add background pattern in Login Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,15 +2,22 @@
 import React from 'react';
 import LoginBox from "../components/LoginBox";
 import Header from "../components/Header";
+import './Main.css'
+import circle from "../assets/images/img_circle.svg";
 
 function Login() {
     return (
-        <>
-            <Header/>
-            <div style={{margin:"60px auto"}}>
+        <div className="main_container">
+            <img className="header_mobile_effectCircleTop" src={circle}/>
+            <img className="header_mobile_effectCircleCenter" src={circle}/>
+            <img className="header_mobile_effectCircleBottom" src={circle}/>
+            <div className="main_header">
+                <Header />
+            </div>
+            <div style={{margin:"160px auto",position:"absolute",width:"100%"}}>
                 <LoginBox/>
             </div>
-        </>
+        </div>
     );
 }
 
