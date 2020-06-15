13efaec1facfc7d8e751da9a03bce0a05dfa62fe
GoldenMine0502 <ehe123@naver.com>
2020-06-15 17:11:58 +0900

    background

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,13 +3,12 @@ import React from 'react';
 import Header from '../components/Header';
 import './Main.css'
 import circle from '../assets/images/img_circle.svg'
+import Background from "../components/Background";
 
 function Main() {
     return (
         <div className="main_container">
-            <img className="header_mobile_effectCircleTop" src={circle}/>
-            <img className="header_mobile_effectCircleCenter" src={circle}/>
-            <img className="header_mobile_effectCircleBottom" src={circle}/>
+            <Background />
             <div className="main_header">
                 <Header />
             </div>
