d4a14560a1840709fda3fc24faabf5799a55c469
GoldenMine0502 <ehe123@naver.com>
2020-06-20 16:23:55 +0900

    login.tsx background is changed to <Background />

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,15 +2,13 @@
 import React from 'react';
 import LoginBox from "../components/LoginBox";
 import Header from "../components/Header";
-import './Main.css'
-import circle from "../assets/images/img_circle.svg";
+import './Main.scss';
+import Background from "../components/Background";
 
 function Login() {
     return (
         <div className="main_container">
-            <img className="header_mobile_effectCircleTop" src={circle}/>
-            <img className="header_mobile_effectCircleCenter" src={circle}/>
-            <img className="header_mobile_effectCircleBottom" src={circle}/>
+            <Background />
             <div className="main_header">
                 <Header />
             </div>
